b12b7388103631c7f4db4e0135079399509a6830
Eugene Kazakov <ekazakov@parallels.com>
2012-03-15 12:25:18 +0600

    rm

--- plib/Bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('APPLICATION_PATH', dirname(__FILE__));
-
-set_include_path(
-    APPLICATION_PATH . '/library/'
-    . PATH_SEPARATOR . get_include_path()
-);
-
-pm_Loader::enableAutoload();
