39db5c45350345011e785d0acd7f79c67849cc08
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-02-25 08:36:54 +0700

    Example of how to use variables in locale messages.

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -7,7 +7,7 @@ class IndexController extends pm_Controller_Action
         parent::init();
 
         // Init title for all actions
-        $this->view->pageTitle = 'Example Module';
+        $this->view->pageTitle = $this->lmsg('pageTitle', ['product' => 'Plesk']);
 
         // Init tabs for all actions
         $this->view->tabs = array(
---
+++ plib/resources/locales/en-US.php
@@ -0,0 +1,5 @@
+<?php
+
+$messages = [
+    'pageTitle' => 'Extension Example for %%product%%',
+];
