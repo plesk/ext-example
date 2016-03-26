367d448abba792e0793956c51685305374104c5a
Alexei Yuzhakov <ayuzhakov@plesk.com>
2016-03-26 14:55:01 +0600

    Use array short syntax across the whole code base.

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -10,20 +10,20 @@ class IndexController extends pm_Controller_Action
         $this->view->pageTitle = $this->lmsg('pageTitle', ['product' => 'Plesk']);
 
         // Init tabs for all actions
-        $this->view->tabs = array(
-            array(
+        $this->view->tabs = [
+            [
                 'title' => 'Form',
                 'action' => 'form',
-            ),
-            array(
+            ],
+            [
                 'title' => 'Tools',
                 'action' => 'tools',
-            ),
-            array(
+            ],
+            [
                 'title' => 'List',
                 'action' => 'list',
-            ),
-        );
+            ],
+        ];
     }
 
     public function indexAction()
@@ -39,60 +39,60 @@ class IndexController extends pm_Controller_Action
 
         // Init form here
         $form = new pm_Form_Simple();
-        $form->addElement('text', 'exampleText', array(
+        $form->addElement('text', 'exampleText', [
             'label' => 'Example Text',
             'value' => pm_Settings::get('exampleText'),
             'required' => true,
-            'validators' => array(
-                array('NotEmpty', true),
-            ),
-        ));
-        $form->addElement('password', 'examplePassword', array(
+            'validators' => [
+                ['NotEmpty', true],
+            ],
+        ]);
+        $form->addElement('password', 'examplePassword', [
             'label' => 'Example Password',
             'value' => '',
             'description' => 'Password: ' . pm_Settings::get('examplePassword'),
-            'validators' => array(
-                array('StringLength', true, array(5, 255)),
-            ),
-        ));
-        $form->addElement('textarea', 'exampleTextarea', array(
+            'validators' => [
+                ['StringLength', true, [5, 255]],
+            ],
+        ]);
+        $form->addElement('textarea', 'exampleTextarea', [
             'label' => 'Example TextArea',
             'value' => pm_Settings::get('exampleTextarea'),
             'class' => 'f-middle-size',
             'rows' => 4,
             'required' => true,
-            'validators' => array(
-                array('StringLength', true, array(0, 255)),
-            ),
-        ));
-        $form->addElement('simpleText', 'exampleSimpleText', array(
+            'validators' => [
+                ['StringLength', true, [0, 255]],
+            ],
+        ]);
+        $form->addElement('simpleText', 'exampleSimpleText', [
             'label' => 'Example SimpleText',
             'escape' => false,
             'value' => '<a href="#">Link</a>',
-        ));
-        $form->addElement('select', 'exampleSelect', array(
+        ]);
+        $form->addElement('select', 'exampleSelect', [
             'label' => 'Example Select',
-            'multiOptions' => array('opt-0' => 'Option 0', 'opt-1' => 'Option 1'),
+            'multiOptions' => ['opt-0' => 'Option 0', 'opt-1' => 'Option 1'],
             'value' => pm_Settings::get('exampleSelect'),
             'required' => true,
-        ));
-        $form->addElement('radio', 'exampleRadio', array(
+        ]);
+        $form->addElement('radio', 'exampleRadio', [
             'label' => 'Example Radio',
-            'multiOptions' => array('opt-0' => 'Option 0', 'opt-1' => 'Option 1'),
+            'multiOptions' => ['opt-0' => 'Option 0', 'opt-1' => 'Option 1'],
             'value' => pm_Settings::get('exampleRadio'),
             'required' => true,
-        ));
-        $form->addElement('checkbox', 'exampleCheckbox', array(
+        ]);
+        $form->addElement('checkbox', 'exampleCheckbox', [
             'label' => 'Example Checkbox',
             'value' => pm_Settings::get('exampleCheckbox'),
-        ));
-        $form->addElement('hidden', 'exampleHidden', array(
+        ]);
+        $form->addElement('hidden', 'exampleHidden', [
             'value' => 'example',
-        ));
+        ]);
 
-        $form->addControlButtons(array(
+        $form->addControlButtons([
             'cancelLink' => pm_Context::getModulesListUrl(),
-        ));
+        ]);
 
         if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
             // Form proccessing here
@@ -106,7 +106,7 @@ class IndexController extends pm_Controller_Action
             pm_Settings::set('exampleCheckbox', $form->getValue('exampleCheckbox'));
 
             $this->_status->addMessage('info', 'Data was successfully saved.');
-            $this->_helper->json(array('redirect' => pm_Context::getBaseUrl()));
+            $this->_helper->json(['redirect' => pm_Context::getBaseUrl()]);
         }
 
         $this->view->form = $form;
@@ -115,36 +115,36 @@ class IndexController extends pm_Controller_Action
     public function toolsAction()
     {
         // Tools for pm_View_Helper_RenderTools
-        $this->view->tools = array(
-            array(
+        $this->view->tools = [
+            [
                 'icon' => pm_Context::getBaseUrl() . 'images/hosting-setup.png',
                 'title' => 'Example',
                 'description' => 'Example module with UI samples',
                 'link' => pm_Context::getBaseUrl(),
-            ),
-            array(
+            ],
+            [
                 'icon' => pm_Context::getBaseUrl() . 'images/databases.png',
                 'title' => 'Modules',
                 'description' => 'Modules installed in the Panel',
                 'link' => pm_Context::getModulesListUrl(),
-            ),
-        );
+            ],
+        ];
 
         // Tools for pm_View_Helper_RenderSmallTools
-        $this->view->smallTools = array(
-            array(
+        $this->view->smallTools = [
+            [
                 'title' => 'Example',
                 'description' => 'Example module with UI samples',
                 'class' => 'sb-app-info',
                 'link' => pm_Context::getBaseUrl(),
-            ),
-            array(
+            ],
+            [
                 'title' => 'Modules',
                 'description' => 'Modules installed in the Panel',
                 'class' => 'sb-suspend',
                 'link' => pm_Context::getModulesListUrl(),
-            ),
-        );
+            ],
+        ];
     }
 
     public function listAction()
@@ -169,17 +169,17 @@ class IndexController extends pm_Controller_Action
             $_SESSION['module']['example']['removed'] = [];
         }
 
-        $data = array();
+        $data = [];
         $iconPath = pm_Context::getBaseUrl() . 'images/icon_16.gif';
         for ($index = 1; $index < 150; $index++) {
             if (in_array($index, $_SESSION['module']['example']['removed'])) {
                 continue;
             }
 
-            $data[$index] = array(
+            $data[$index] = [
                 'column-1' => '<a href="#">link #' . $index . '</a>',
                 'column-2' => '<img src="' . $iconPath . '" /> image #' . $index,
-            );
+            ];
         }
 
         $options = [
@@ -188,19 +188,19 @@ class IndexController extends pm_Controller_Action
         ];
         $list = new pm_View_List_Simple($this->view, $this->_request, $options);
         $list->setData($data);
-        $list->setColumns(array(
+        $list->setColumns([
             pm_View_List_Simple::COLUMN_SELECTION,
-            'column-1' => array(
+            'column-1' => [
                 'title' => 'Link',
                 'noEscape' => true,
                 'searchable' => true,
-            ),
-            'column-2' => array(
+            ],
+            'column-2' => [
                 'title' => 'Description',
                 'noEscape' => true,
                 'sortable' => false,
-            ),
-        ));
+            ],
+        ]);
         $list->setTools([
             [
                 'title' => 'Hide',
@@ -225,7 +225,7 @@ class IndexController extends pm_Controller_Action
             ],
         ]);
         // Take into account listDataAction corresponds to the URL /list-data/
-        $list->setDataUrl(array('action' => 'list-data'));
+        $list->setDataUrl(['action' => 'list-data']);
 
         return $list;
     }
