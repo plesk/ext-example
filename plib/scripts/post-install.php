e9f914837b70b82334aaee5aef92bc12222848a8
Alexei Yuzhakov <ayuzhakov@plesk.com>
2016-03-26 14:15:48 +0600

    Remove example of deprecated way of adding the custom buttons.

--- plib/scripts/post-install.php
+++ plib/scripts/post-install.php
@@ -3,59 +3,19 @@
 pm_Context::init('example');
 
 // This code is just an example of post-install script, do not use it in production
-// See https://github.com/plesk/ext-custom-buttons for custom buttons integration
 
 if (false !== ($upgrade = array_search('upgrade', $argv))) {
     $upgradeVersion = $argv[$upgrade + 1];
     echo "upgrading from version $upgradeVersion\n";
 
     if (version_compare($upgradeVersion, '1.2') < 0) {
-        pm_Bootstrap::init();
-        $id = pm_Bootstrap::getDbAdapter()->fetchOne("select val from misc where param = 'moduleExampleCustomButton'");
-        pm_Bootstrap::getDbAdapter()->delete('misc', array("param = 'moduleExampleCustomButton'"));
-
-        pm_Settings::set('customButtonId', $id);
+        pm_Settings::set('history', 'upgrade');
     }
 
-    echo "done\n";
+    echo "Upgrade finished.\n";
     exit(0);
 }
 
-$iconPath = rtrim(pm_Context::getHtdocsDir(), '/') . '/images/icon_16.gif';
-$baseUrl = pm_Context::getBaseUrl();
-
-$request = <<<APICALL
-<ui>
-   <create-custombutton>
-         <owner>
-            <admin/>
-         </owner>
-      <properties>
-         <file>$iconPath</file>
-         <internal>true</internal>
-         <noframe>true</noframe>
-         <place>navigation</place>
-         <url>$baseUrl</url>
-         <text>Example Module</text>
-      </properties>
-   </create-custombutton>
-</ui>
-APICALL;
-
-try {
-    $response = pm_ApiRpc::getService()->call($request);
-
-    $result = $response->ui->{"create-custombutton"}->result;
-    if ('ok' == $result->status) {
-        pm_Settings::set('customButtonId', $result->id);
-        echo "done\n";
-        exit(0);
-    } else {
-        echo "error $result->errcode: $result->errtext\n";
-        exit(1);
-    }
-
-} catch (PleskAPIParseException $e) {
-    echo $e->getMessage() . "\n";
-    exit(1);
-}
+pm_Settings::set('history', 'install');
+echo "Installation finished.\n";
+exit(0);
--- plib/scripts/pre-uninstall.php
+++ plib/scripts/pre-uninstall.php
@@ -3,33 +3,6 @@
 pm_Context::init('example');
 
 // This code is just an example of pre-uninstall script, do not use it in production
-// See https://github.com/plesk/ext-custom-buttons for custom buttons integration
 
-$id = pm_Settings::get('customButtonId');
+pm_Settings::clean();
 
-$request = <<<APICALL
-<ui>
-    <delete-custombutton>
-        <filter>
-            <custombutton-id>$id</custombutton-id>
-        </filter>
-    </delete-custombutton>
-</ui>
-APICALL;
-
-try {
-    $response = pm_ApiRpc::getService()->call($request);
-
-    $result = $response->ui->{"delete-custombutton"}->result;
-    if (true || 'ok' == $result->status) {
-        echo "done\n";
-        exit(0);
-    } else {
-        echo "error $result->errcode: $result->errtext\n";
-        exit(1);
-    }
-
-} catch (PleskAPIParseException $e) {
-    echo $e->getMessage() . "\n";
-    exit(1);
-}
