e46609f9006df3e5291685c3dabd3be7450de9cb
Yulia Kuzakova <y.g.kuzakova@gmail.com>
2016-05-16 12:12:48 +0600

    Add new attribute 'type' for active list item with values (danger, warning, success, disabled). Replace 'ColumnsNumber' option to 'layout' option.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -8,11 +8,15 @@ $messages = [
     'pageTitle' => 'Extension Example for %%product%%',
     'service1Title' => 'Service title1',
     'service2Title' => 'Service title2',
+    'service4Title' => 'Service title3',
     'status1Title' => 'Status Active',
     'status2Title' => 'Status Inactive',
+    'status3Title' => 'Warning',
+    'status4Title' => 'Stopped',
     'param1Title' => 'ParamTitle1',
     'param2Title' => 'ParamTitle2',
     'param3Title' => 'ParamTitle3',
+    'param4Title' => 'ParamTitle4',
     'action1Title' => 'Start Service',
     'action2Title' => 'Stop Service',
     'action3Title' => 'Restart Service',
@@ -22,4 +26,4 @@ $messages = [
     'activeListHeader' => 'Your Containers',
     'htmlBlock' => 'Some html block for data',
     'toolbarTitle' => 'toolbar in service',
-];
+   ];
