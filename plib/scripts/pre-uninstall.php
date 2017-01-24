ee5492c9d80b55c239ba06294b43ed13eb84ea12
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-01-24 21:38:25 +0700

    Update copyright year.

--- plib/scripts/pre-uninstall.php
+++ plib/scripts/pre-uninstall.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2016. Parallels IP Holdings GmbH.
+// Copyright 1999-2017. Parallels IP Holdings GmbH.
 
 // This code is just an example of pre-uninstall script, do not use it in production
 
